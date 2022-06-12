397fb6635200d669cf33e090a8cda6a36f0b00cd
xyx266617 <62925356+xyx266617@users.noreply.github.com>
2022-06-12 13:16:46 +0800

    完成设置界面

--- app/src/main/java/com/example/voicerecord/Activity/VoicePlayActivity.kt
+++ app/src/main/java/com/example/voicerecord/Activity/VoicePlayActivity.kt
@@ -1,5 +1,9 @@
 package com.example.voicerecord.Activity
 
+import android.app.AlertDialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.Paint
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,8 +12,20 @@ import android.os.Looper
 import android.os.Message
 import android.util.Log
 import android.view.View
+import android.view.animation.LinearInterpolator
 import android.widget.SeekBar
+import com.example.voicerecord.R
 import com.example.voicerecord.databinding.ActivityVoicePlayBinding
+import me.bogerchan.niervisualizer.NierVisualizerManager
+import me.bogerchan.niervisualizer.renderer.IRenderer
+import me.bogerchan.niervisualizer.renderer.circle.CircleBarRenderer
+import me.bogerchan.niervisualizer.renderer.circle.CircleRenderer
+import me.bogerchan.niervisualizer.renderer.circle.CircleSolidRenderer
+import me.bogerchan.niervisualizer.renderer.circle.CircleWaveRenderer
+import me.bogerchan.niervisualizer.renderer.columnar.*
+import me.bogerchan.niervisualizer.renderer.line.LineRenderer
+import me.bogerchan.niervisualizer.renderer.other.ArcStaticRenderer
+import me.bogerchan.niervisualizer.util.NierAnimator
 import java.io.File
 import kotlin.concurrent.thread
 
@@ -19,13 +35,15 @@ class VoicePlayActivity : AppCompatActivity() {
     var duration:Int=0
     var mMediaPlayer:MediaPlayer?=null
     var isPlaying:Boolean=false
-    var isLoop:Boolean=true
+    var isLoop:Boolean=false
     val updateTimeNow=1
     val updateSeekBar=2
     val playOver=3
     var isOver=false
     var speed=Array<Float>(8){i->0f}
     var speedLevel=3
+    lateinit var effect: Array<IRenderer>
+    val visualizerManager = NierVisualizerManager()
     val handler=object: Handler(Looper.getMainLooper()){
         override fun handleMessage(msg: Message){
             when(msg.what){
@@ -44,6 +62,9 @@ class VoicePlayActivity : AppCompatActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        if (getSupportActionBar() != null){
+            getSupportActionBar()?.hide();
+        }
         super.onCreate(savedInstanceState)
         binding= ActivityVoicePlayBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -58,15 +79,16 @@ class VoicePlayActivity : AppCompatActivity() {
         speed[5]=1.5f
         speed[6]=2.0f
         speed[7]=3.0f
-//        speed.add(2,0.8f)
-//        speed.add(3,1.0f)
-//        speed.add(4,1.25f)
-//        speed.add(5,1.5f)
-//        speed.add(6,2f)
-//        speed.add(7,3f)
         mMediaPlayer=MediaPlayer()
         mMediaPlayer?.setDataSource(file.absolutePath)
         mMediaPlayer?.prepare()
+
+        getEffectType()
+
+        val state = mMediaPlayer?.audioSessionId?.let { visualizerManager.init(it) }
+        if (NierVisualizerManager.SUCCESS != state) {
+            // 可以进行一些错误处理...
+        }
         duration= mMediaPlayer!!.duration//单位 ms
         binding.voicePlayTimeEnd.setText(timeFormat(duration/1000))
         playVoice()
@@ -85,6 +107,120 @@ class VoicePlayActivity : AppCompatActivity() {
         }
     }
 
+    //获取用户选择的可视化特效
+    fun getEffectType(){
+        val preferences=getSharedPreferences("config", Context.MODE_PRIVATE)
+        val effect_type=preferences.getInt("effect_type",7)
+        when(effect_type){
+            1->{
+                effect= arrayOf(ColumnarType1Renderer())
+            }
+            2->{
+                effect= arrayOf(ColumnarType2Renderer())
+            }
+            3->{
+                effect= arrayOf(ColumnarType3Renderer())
+            }
+            4->{
+                effect= arrayOf(ColumnarType4Renderer())
+            }
+            5->{
+                effect= arrayOf(LineRenderer(true))
+            }
+            6->{
+                effect= arrayOf(CircleBarRenderer())
+            }
+            7->{
+                effect= arrayOf(CircleRenderer(true))
+            }
+            8->{
+                effect= arrayOf(CircleRenderer(true),CircleBarRenderer())
+            }
+            9->{
+                effect= arrayOf(CircleRenderer(true),CircleBarRenderer(),LineRenderer(true))
+            }
+            10->{
+                effect= arrayOf(
+                    ArcStaticRenderer(
+                        paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+                            color = Color.parseColor("#cfa9d0fd")
+                        }),
+                    ArcStaticRenderer(
+                        paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+                            color = Color.parseColor("#dad2eafe")
+                        },
+                        amplificationOuter = .83f,
+                        startAngle = -90f,
+                        sweepAngle = 225f
+                    ),
+                    ArcStaticRenderer(
+                        paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+                            color = Color.parseColor("#7fa9d0fd")
+                        },
+                        amplificationOuter = .93f,
+                        amplificationInner = 0.8f,
+                        startAngle = -45f,
+                        sweepAngle = 135f
+                    ),
+                    CircleSolidRenderer(
+                        paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+                            color = Color.parseColor("#d2eafe")
+                        },
+                        amplification = .45f
+                    ),
+                    CircleBarRenderer(
+                        paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+                            strokeWidth = 4f
+                            color = Color.parseColor("#efe3f2ff")
+                        },
+                        modulationStrength = 1f,
+                        type = CircleBarRenderer.Type.TYPE_A_AND_TYPE_B,
+                        amplification = 1f, divisions = 8
+                    ),
+                    CircleBarRenderer(
+                        paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+                            strokeWidth = 5f
+                            color = Color.parseColor("#e3f2ff")
+                        },
+                        modulationStrength = 0.1f,
+                        amplification = 1.2f,
+                        divisions = 8
+                    ),
+                    CircleWaveRenderer(
+                        paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+                            strokeWidth = 6f
+                            color = Color.WHITE
+                        },
+                        modulationStrength = 0.2f,
+                        type = CircleWaveRenderer.Type.TYPE_B,
+                        amplification = 1f,
+                        animator = NierAnimator(
+                            interpolator = LinearInterpolator(),
+                            duration = 20000,
+                            values = floatArrayOf(0f, -360f)
+                        )
+                    ),
+                    CircleWaveRenderer(
+                        paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+                            strokeWidth = 6f
+                            color = Color.parseColor("#7fcee7fe")
+                        },
+                        modulationStrength = 0.2f,
+                        type = CircleWaveRenderer.Type.TYPE_B,
+                        amplification = 1f,
+                        divisions = 8,
+                        animator = NierAnimator(
+                            interpolator = LinearInterpolator(),
+                            duration = 20000,
+                            values = floatArrayOf(0f, -360f)
+                        )
+                    )
+
+                )
+            }
+        }
+    }
+
     fun playVoice(){
         if(mMediaPlayer?.currentPosition==duration){
             rePlay()
@@ -92,6 +228,7 @@ class VoicePlayActivity : AppCompatActivity() {
         else {
             setSpeed()
             mMediaPlayer?.start()
+            visualizerManager.start(binding.svWave, effect)
             isPlaying = true
             binding.voicePlayButtonPause.visibility = View.VISIBLE
             binding.voicePlayButtonPlay.visibility = View.INVISIBLE
@@ -130,6 +267,21 @@ class VoicePlayActivity : AppCompatActivity() {
         binding.voicePlaySpeedDown.setOnClickListener(){
             changeSpeedLevel(-1)
         }
+        binding.buttonDeletePlay.setOnClickListener(){
+            showDeleteDialog()
+        }
+        binding.buttonBackPlay.setOnClickListener(){
+            finish()
+        }
+        binding.voicePlayButtonReset.setOnClickListener(){
+            rePlay()
+        }
+        binding.voicePlayButtonLoop.setOnClickListener(){
+            changeLoop()
+        }
+        binding.voicePlayButtonLoopNot.setOnClickListener(){
+            changeLoop()
+        }
 
         binding.voicePlaySeekBar.setOnSeekBarChangeListener(object :
             SeekBar.OnSeekBarChangeListener {
@@ -154,8 +306,27 @@ class VoicePlayActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         mMediaPlayer?.release()
+        visualizerManager.release()
         isOver=true;
         isPlaying=false;
+    }
+
+    fun showDeleteDialog(){
+        val builder= AlertDialog.Builder(this)
+        builder.setTitle("提示")
+        builder.setMessage("您确定要删除当前录音吗？")
+
+        builder.setPositiveButton(
+            "确定") { dialog, id ->
+            mMediaPlayer?.release()
+            mMediaPlayer=null
+            val file=File(filename)
+            file.delete()
+            finish()
+        }
+        builder.setNegativeButton("取消",null)
+        val dialog=builder.create()
+        dialog.show()
     }
 
     fun changeSpeedLevel(value: Int){
@@ -194,6 +365,20 @@ class VoicePlayActivity : AppCompatActivity() {
                     Thread.sleep(200)
                 }
             }
+        }
+    }
+
+    //修改循环播放状态
+    fun changeLoop(){
+        if(binding.voicePlayButtonLoop.visibility.equals(View.VISIBLE)){
+            binding.voicePlayButtonLoop.visibility=View.INVISIBLE
+            binding.voicePlayButtonLoopNot.visibility=View.VISIBLE
+            isLoop=false
+        }
+        else if(binding.voicePlayButtonLoopNot.visibility.equals(View.VISIBLE)){
+            binding.voicePlayButtonLoop.visibility=View.VISIBLE
+            binding.voicePlayButtonLoopNot.visibility=View.INVISIBLE
+            isLoop=true
         }
     }
 
--- app/src/main/java/com/example/voicerecord/Fragment/SettingFragment.kt
+++ app/src/main/java/com/example/voicerecord/Fragment/SettingFragment.kt
@@ -1,14 +1,24 @@
 package com.example.voicerecord.Fragment
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.voicerecord.Adapter.VoiceListAdapter
 import com.example.voicerecord.R
+import java.io.File
 
 class SettingFragment : Fragment() {
 
+    var effect_id:Int=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -21,4 +31,53 @@ class SettingFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_setting, container, false)
     }
 
+    override fun onStart() {
+        super.onStart()
+        val preferences=activity?.getSharedPreferences("config", Context.MODE_PRIVATE)
+        val effect_type=preferences?.getInt("effect_type",7)
+        when(effect_type){
+            1->{effect_id=R.id.visualize_effect_1}
+            2->{effect_id=R.id.visualize_effect_2}
+            3->{effect_id=R.id.visualize_effect_3}
+            4->{effect_id=R.id.visualize_effect_4}
+            5->{effect_id=R.id.visualize_effect_5}
+            6->{effect_id=R.id.visualize_effect_6}
+            7->{effect_id=R.id.visualize_effect_7}
+            8->{effect_id=R.id.visualize_effect_8}
+            9->{effect_id=R.id.visualize_effect_9}
+            10->{effect_id=R.id.visualize_effect_10}
+        }
+        val effectRadioGroup=activity?.findViewById<RadioGroup>(R.id.visualize_effect_group)
+        effectRadioGroup?.check(effect_id)
+        effectRadioGroup?.setOnCheckedChangeListener(
+            { radioGroup, i ->
+                effect_id=i
+            }
+        )
+        activity?.findViewById<Button>(R.id.button_setting_save)?.setOnClickListener(){
+            save()
+        }
+
+    }
+
+    fun save(){
+        var effect_type:Int=0;
+        when(effect_id){
+            R.id.visualize_effect_1->{effect_type=1}
+            R.id.visualize_effect_2->{effect_type=2}
+            R.id.visualize_effect_3->{effect_type=3}
+            R.id.visualize_effect_4->{effect_type=4}
+            R.id.visualize_effect_5->{effect_type=5}
+            R.id.visualize_effect_6->{effect_type=6}
+            R.id.visualize_effect_7->{effect_type=7}
+            R.id.visualize_effect_8->{effect_type=8}
+            R.id.visualize_effect_9->{effect_type=9}
+            R.id.visualize_effect_10->{effect_type=10}
+        }
+        val preferences=activity?.getSharedPreferences("config", Context.MODE_PRIVATE)
+        val editor = preferences?.edit()
+        editor?.clear()
+        editor?.putInt("effect_type",effect_type)
+        editor?.apply()
+    }
 }
